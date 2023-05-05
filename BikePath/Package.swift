eb0b7a5c49d2fc9698b88face1ba231d5c7d769b
David Albert <davidbalbert@gmail.com>
2023-05-05 11:25:21 -0400

    Remove swift-parsing dependency

--- BikePath/Package.swift
+++ BikePath/Package.swift
@@ -15,15 +15,12 @@ let package = Package(
             name: "BikePath",
             targets: ["BikePath"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.7.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BikePath",
-            dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
+            dependencies: []),
         .testTarget(
             name: "BikePathTests",
             dependencies: ["BikePath"]),
